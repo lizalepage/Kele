6ad2e99ac0a55c1b8fc3f274593f55dac95795e5
EC2 Default User <ec2-user@ip-172-31-88-173.ec2.internal>
2018-07-19 02:27:42 +0000

    current-user-added

--- lib/kele.rb
+++ lib/kele.rb
@@ -2,15 +2,21 @@ require 'httparty'
 require 'json'
 
 class Kele
-  include HTTParty
-  base_uri "https://www.bloc.io/api/v1/"
+    include HTTParty
+    base_uri "https://www.bloc.io/api/v1/"
 
-  def initialize(email, password)
-    response = self.class.post(api_url("sessions"), body: { email: email, password: password })
-    raise 'Invalid email or password' if response.code == 404
-    @auth_token = response["auth_token"]
+    def initialize(email, password)
+        response = self.class.post(api_url("sessions"), body: { email: email, password: password })
+        raise 'Invalid email or password' if response.code == 404
+        @auth_token = response["auth_token"]
 
-  end
+    end
+  
+    def get_me
+        response = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+        @user = JSON.parse(response.body)
+        
+    end      
 
   private
 
