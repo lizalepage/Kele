5d7d47da2f605d32abd1f76a0d28bedde3a523e9
EC2 Default User <ec2-user@ip-172-31-88-173.ec2.internal>
2018-07-23 20:08:17 +0000

    roadmaps and checkpoints working

--- lib/roadmap.rb
+++ lib/roadmap.rb
@@ -1,11 +1,13 @@
 module Roadmap
 
-    def get_roadmap
-    
+    def get_roadmap(chain_id)
+        response = self.class.get(api_url("roadmaps/#{chain_id}"),  headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end
     
-    def get_checkpoint
-    
+    def get_checkpoint(checkpoint_id)
+        response = self.class.get(api_url("checkpoints/#{checkpoint_id}"), headers: { "authorization" => @auth_token })
+        JSON.parse(response.body)
     end    
 
 end    
